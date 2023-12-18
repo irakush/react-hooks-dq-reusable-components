cfa0942b365c9aa14eb95d831b188b50510a00a6
Igor Rakush <irakush@aol.com>
2023-12-18 11:36:24 -0500

    done

--- src/components/ShopTile.js
+++ src/components/ShopTile.js
@@ -3,10 +3,10 @@ import React from "react";
 // Study me! What information do I require to work?
 // How am I the same as InventoryTile? How am I different?
 
-function ShopTile({ potion, addToInventory, children }) {
+function ShopTile({ potion, stateFunc, children }) {
   return (
     <div className="card">
-      <div onClick={() => addToInventory(potion.id)} className="image-wrapper">
+      <div onClick={() => stateFunc(potion.id)} className="image-wrapper">
         <img className="image" alt={potion.name} src={potion.image_url} />
       </div>
       {children}
